cec1737a97f20017c6e84622485fec99999e5551
Ben Mason <su1droot@gmail.com>
2014-04-24 20:01:33 -0400

    Added Tables, cleaned CSV and code optimization

--- networkstatesystem/classes.py
+++ networkstatesystem/classes.py
@@ -1,22 +1,242 @@
 
+import json
+import prettytable
+
 class device(object):
 	"""
 
 		Definitaion of a device
 
 	"""
-	def __init__(self, hostname, ipaddress, datasource, routingtable, interfacetable):
+	def __init__(self, hostname, osversion, ipaddress, datasource, routingtable, interfacetable):
 		#super(device, self).__init__()
 		self.hostname = hostname
+		self.osversion = osversion
 		self.ipaddress = ipaddress
 		self.datasource = datasource
 		self.routingtable = routingtable
 		self.interfacetable = interfacetable
 
-	def printinfo(self):
-		print "Hostname: {0}".format(self.hostname) 
-		print "IP Address {0}".format(self.ipaddress)
-		print "Data Source: {0}".format(self.datasource)
-		print "Routing Table: {0}".format(self.routingtable)
-		print "Interfaces: {0}".format(self.interfacetable)
+
+
+	def printinterfacestats(self, format):
+
+		interfacedata = self.interfacetable
+		hostname = self.hostname
+
+		ignoreoids = [22, 21, 18, 12]
 		
+		ifmib = {
+				1:"ifIndex",
+				2:"ifDescr",
+				3:"ifType",
+				4:"ifMtu",
+				5:"ifSpeed",
+				6:"ifPhysAddress",
+				7:"ifAdminStatus",
+				8:"ifOperStatus",
+				9:"ifLastChange",
+				10:"ifInOctets",
+				11:"ifUcastPkts",
+				12:"ifInNUcastPkts",
+				13:"ifInDiscards",
+				14:"ifInErrors",
+				15:"ifInUnknownProtos",
+				16:"ifOutOctets",
+				17:"ifOutUcastPkts",
+				18:"ifOutNUcastPkts",
+				19:"ifOutDiscards",
+				20:"ifOutErrors",
+				21:"ifOutQLen",
+				22:"ifSpecific"
+		}
+		
+		# oid value of 8 or 7
+		ifstatus = {
+				1: "up",
+				2: "down",
+				3: "testing",
+				4: "unknown",
+				5: "dormant",
+				6: "notPresent",
+				7: "lowerLayerDown"
+		}
+
+		
+		if format == 'csv':
+			headerrow = []
+			headerrow.append("hostname")
+						
+			for id in sorted(ifmib):
+				if id not in ignoreoids:
+					headerrow.append(ifmib[id])
+
+			print ",".join(headerrow)
+
+			for ifindex in sorted(interfacedata):
+				currentrow = []
+				currentrow.append(hostname)
+				for oid in sorted(interfacedata[ifindex]):
+					for skip in ignoreoids:
+						if oid == skip:
+							exclude = True
+							break
+						else:
+							exclude = False
+					if exclude != True:		
+						if oid == 8 or oid == 7:
+							currentrow.append(ifstatus[int(interfacedata[ifindex][oid])])
+						else:
+							currentrow.append(interfacedata[ifindex][oid])
+				print ",".join(currentrow)
+
+		elif format == 'table':
+			headerrow = []
+			headerrow.append("hostname")
+						
+			for id in sorted(ifmib):
+				if id not in ignoreoids:
+					headerrow.append(ifmib[id])
+
+			thetable = prettytable.PrettyTable(headerrow)
+
+			for ifindex in sorted(interfacedata):
+				currentrow = [hostname]
+				for oid in sorted(interfacedata[ifindex]):
+					if oid not in ignoreoids:
+						if oid == 8 or oid == 7:
+							currentrow.append(ifstatus[int(interfacedata[ifindex][oid])])
+						else:
+							currentrow.append(interfacedata[ifindex][oid])
+				thetable.add_row(currentrow)
+			print thetable			
+
+		else:
+		# Print Detailed textual list of interface information
+			for ifindex in sorted(interfacedata):
+				print "\nInterface Number", ifindex
+				for oid in sorted(interfacedata[ifindex]):
+					if oid not in ignoreoids:
+						print "{0} ({1}) =".format(ifmib[oid], oid), 
+				
+						if oid == 8 or oid == 7:
+							print ifstatus[int(interfacedata[ifindex][oid])]
+						# Need to fid display of MAC Addresses
+						#elif oid == 6:
+						#	print "{0}".format(interfacedata[ifindex][oid].hexdigits)
+						else:
+							print interfacedata[ifindex][oid]
+				print "\n",
+
+	def printinterfacesumary(self, format):
+
+		interfacedata = self.interfacetable
+		hostname = self.hostname
+		#format = 'text'
+
+		numberup = 0
+		numberdown = 0
+		status = 0
+		
+		for ifindex in sorted(interfacedata):
+			if interfacedata[ifindex][7] < interfacedata[ifindex][8]:
+				status = interfacedata[ifindex][8]
+			else:
+				status = interfacedata[ifindex][7]
+					
+			if status == '1':
+				numberup = int(numberup + 1)
+			else:
+				numberdown = int(numberdown + 1)
+
+		percentfree = (numberdown / (numberup + numberdown))*100
+		
+		if format == 'csv':
+			print 'hostname,up,down,percent'
+			print '{0},{1},{2},{3}'.format(hostname, numberup, numberdown, percentfree)
+		else:
+			print "Hostname: {0}".format(hostname)			
+			print "Total Number up: {0}".format(numberup)
+			print "Total Number down: {0}".format(numberdown)
+			print "Percent Free: {0}".format(percentfree)
+
+	def printroutingtable (self, format):
+
+		routingtable = self.routingtable
+
+		ignoreoids = [8,11,12,13,14,15]
+
+		ipCidrRouteEntry = {
+			1:"ipCidrRouteDest",
+			10:"ipCidrRouteNextHopAS",
+			11:"ipCidrRouteMetric1",
+			12:"ipCidrRouteMetric2",
+			13:"ipCidrRouteMetric3",
+			14:"ipCidrRouteMetric4",
+			15:"ipCidrRouteMetric5",
+			16:"ipCidrRouteStatus",
+			2:"ipCidrRouteMask",
+			3:"ipCidrRouteTos",
+			4:"ipCidrRouteNextHop",
+			5:"ipCidrRouteIfIndex",
+			6:"ipCidrRouteType",
+			7:"ipCidrRouteProto",
+			8:"ipCidrRouteAge",
+			9:"ipCidrRouteInfo"
+		}
+
+		headerrow = []
+		headerrow.append("hostname")
+					
+		for id in sorted(ipCidrRouteEntry):
+			if id not in ignoreoids:
+				headerrow.append(ipCidrRouteEntry[id])
+
+		if format == 'csv':
+			print ",".join(headerrow)
+			for routeid in sorted(routingtable):
+				currentrow = [self.hostname]
+				for oid in sorted(routingtable[routeid]):
+					if oid not in ignoreoids:
+						currentrow.append(routingtable[routeid][oid])
+				print ",".join(currentrow)
+
+		else:
+			thetable = prettytable.PrettyTable(headerrow)
+
+			for routeid in sorted(routingtable):
+				currentrow = [self.hostname]
+				for oid in sorted(routingtable[routeid]):
+					if oid not in ignoreoids:
+						currentrow.append(routingtable[routeid][oid])
+				thetable.add_row(currentrow)
+
+			print thetable
+
+	def printallinfo(self, format):
+		print "Hostname: {0}".format(self.hostname) 
+		print "Version: {0}".format(self.osversion)
+		print "IP Address: {0}".format(self.ipaddress)
+		print "Data Source: {0}".format(self.datasource)
+		print "\mRouting Table"
+
+		device.printroutingtable(self, 'table')
+
+		print "\nInterface Infomration"
+
+		device.printinterfacestats(self, 'table')
+
+
+	def returnjson(self):
+
+		device = {}
+
+		device['hostname'] = self.hostname
+		device['osversion'] = self.osversion
+		device['ipaddress'] = self.ipaddress
+		device['datasource'] = self.datasource
+		device['routingtable'] = self.routingtable
+		device['interfacetable'] = self.interfacetable
+
+		#print device
+		return json.dumps(device, sort_keys=True, indent=4, separators=(',', ': '))
--- networkstatesystem/main.py
+++ networkstatesystem/main.py
@@ -9,9 +9,9 @@ import csv
 
 # Network Libraries
 from classes import *
-from vendors.snmp.querygeneraldata import *
-from vendors.snmp.interfacedump import *
-from vendors.snmp.routingdump import *
+from vendors.snmp.querydevicedata import *
+#from vendors.snmp.interfacedump import *
+#from vendors.snmp.routingdump import *
 
 
 def initArgs():
@@ -20,25 +20,30 @@ def initArgs():
 
 	parser.add_argument('-i', '--ip_address', help='i.e. -i "192.168.31.21"')
 	parser.add_argument('-c', '--community', help='Enter SNMP Community')
+	parser.add_argument('-d', '--datasource', help='Data Source (snmp)')
 	parser.add_argument('-f', '--file', help='Load Host list from File')
 	parser.add_argument('-s', '--ifcount', help='Summary Interface Inventory', action='store_true')
-	parser.add_argument('-a', '--detail', help='Detailed Interface Inventory', action='store_true')
-	parser.add_argument('-b', '--csv', help='CSV Format', action='store_true')
+	parser.add_argument('-t', '--ifdetail', help='Detailed Interface Inventory', action='store_true')
+	parser.add_argument('-r', '--routedetail', help='Detailed Routing Table', action='store_true')
+	parser.add_argument('-a', '--allinfo', help='All Device Information', action='store_true')
+	parser.add_argument('-b', '--format', help='Output Format ("csv", "json", or "text")')
 	parser.add_argument('-o', '--output', help='Output to file')
 
 	arg = parser.parse_args()
 
 	hosts = []
 
+	toggles = {}
+
 	# Gather host information from File or Commandline
 	if arg.file:
 		with open(arg.file, "r") as host_list:
 			for line in host_list:
-				(device, community) = string.split(line,':')
+				(device, community,datasource) = string.split(line,':')
 				if device[0] == "#":
 					pass
 				else:
-					hosts.append([device, community.rstrip()])
+					hosts.append([device, community.rstrip(), datasource])
 	else:
 		# set ip address to make calls on
 		if arg.ip_address:
@@ -52,69 +57,83 @@ def initArgs():
 		else:
 			sys.exit("You should specify a community")
 			#community = 'poopie'
+		if arg.datasource:
+			datasource = arg.datasource
+		else:
+			datasource = 'snmp'
 		
-#		hosts = [device, community]
-		hosts.append([device, community.rstrip()])
-
+		hosts.append([device, community.rstrip(), datasource])
 
 	# Parse other options
 	if arg.ifcount:
-		ifcount = True
+		toggles['ifcount'] = True
 	else: 
-		ifcount = False
+		toggles['ifcount'] = False
 		
-	if arg.detail:
-		detail = True
+	if arg.ifdetail:
+		toggles['ifdetail'] = True
 	else:
-		detail = False
+		toggles['ifdetail'] = False
+
+	if arg.routedetail:
+		toggles['routedetail'] = True
+	else:
+		toggles['routedetail'] = False
+
+	if arg.allinfo:
+		toggles['allinfo'] = True
+	else:
+		toggles['allinfo'] = False
+
 	
-	if arg.csv:
-		format = 'csv'
+	if arg.format == 'csv':
+		toggles['format'] = 'csv'
+	elif arg.format == 'json':
+		toggles['format'] = 'json'
+	elif arg.format == 'table':
+		toggles['format'] = 'table'
 	else:
-		format = 'text'
+		toggles['format'] = 'text'
 
 	if arg.output:
-		outputfile = arg.output
+		toggles['output'] = arg.output
 	else:
-		outputfile = ""
+		toggles['output'] = ""
 
-	return hosts, ifcount, detail, format, outputfile
+	return hosts, toggles
 
 
 if __name__ == "__main__":
 ##### Start Main Section ######
 
-	host_list = []
-	host_list, ifcount, detail, format, outputfile = initArgs()
+	host_list, toggles = initArgs()
 
 	device_detail_list = {}
 	
 	# Collect information from host_list
-	for ipaddress, community in host_list:
+	for ipaddress, community, datasource in host_list:
 
-		if format == 'text':
-			print "Gathering SNMP Data for, %s using the community %s" % (ipaddress, community)
+		if toggles['format'] == 'text':
+			print "Gathering SNMP Data for, {0} using the community {1}\n\n".format(ipaddress, community)
 
-
-		hostname = hostinfo(ipaddress, community)
+		hostname = gethostname(ipaddress, community)
+		osversion = getosversion(ipaddress, community)
 		interfacedata = populateifdata(ipaddress, community)
 		routingdata = collectroutingtable(ipaddress, community)
+
 		# Collect routing info
+		device_detail_list[ipaddress] = device(hostname, osversion, ipaddress, datasource, routingdata, interfacedata)
 
-		device_detail_list[ipaddress] = device(hostname, ipaddress, 'snmp', routingdata, interfacedata)
-
-		"""print device_detail_list[ipaddress].hostname
-		print device_detail_list[ipaddress].ipaddress
-		print device_detail_list[ipaddress].datasource
-		print device_detail_list[ipaddress].routingtable
-		print device_detail_list[ipaddress].interfacetable"""
-
-		device_detail_list[ipaddress].printinfo()
-
-
-
-		"""if ifcount == True:
-			printinterfacesumary(interfacedata, format, hostname)
-		if detail == True:
-			printinterfacestats(interfacedata, format, hostname)"""
+		
+		if toggles['ifcount'] == True:
+			device_detail_list[ipaddress].printinterfacesumary(toggles['format'])
+		if toggles['ifdetail'] == True:
+			device_detail_list[ipaddress].printinterfacestats(toggles['format'])
+		if toggles['routedetail'] == True:
+			device_detail_list[ipaddress].printroutingtable(toggles['format'])
+		if toggles['allinfo'] == True:
+			if toggles['format'] == 'json':
+				print device_detail_list[ipaddress].returnjson()
+			elif toggles['format'] == 'text':
+				device_detail_list[ipaddress].printallinfo(toggles['format'])
 
